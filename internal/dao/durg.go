11f646362686472373ca94d8ee2ab549351d433c
Xiaodan.C <CisaCoder@gmail.com>
2019-08-29 10:16:16 +0800

    update

--- internal/dao/durg.go
+++ internal/dao/durg.go
@@ -7,23 +7,19 @@ import (
 )
 
 const (
-	//_drug = "SELECT a.*, b.approval FROM `drug` as a, " +
-	//	"detail as b WHERE (a.drug_name LIKE ? OR a.manufacturer LIKE ? OR a." +
-	//	"provider_name LIKE ? OR b. approval LIKE ?) AND a.wholesale_id = b.wholesale_id LIMIT ? OFFSET ?"
+	_drug = "SELECT a.*, b.approval FROM `drug` as a, " +
+		"detail as b WHERE (a.drug_name LIKE ? OR a.manufacturer LIKE ? OR a." +
+		"provider_name LIKE ? OR b. approval LIKE ?) AND a.wholesale_id = b.wholesale_id LIMIT ? OFFSET ?"
 
-	_drug = "SELECT * FROM `drug` WHERE (drug_name LIKE ? OR manufacturer LIKE ? OR provider_name LIKE ? ) LIMIT ? OFFSET ?"
-
-	//_count = "SELECT COUNT(*) FROM `drug` as a, " +
-	//	"detail as b WHERE (a.Drug_name LIKE ? OR a. manufacturer LIKE ? OR a." +
-	//	"provider_name LIKE ? OR b. approval LIKE ?) AND a.wholesale_id = b.wholesale_id"
-
-	_count = "SELECT COUNT(*) FROM `drug` WHERE (drug_name LIKE ? OR manufacturer LIKE ? OR provider_name LIKE ?)"
+	_count = "SELECT COUNT(*) FROM `drug` as a, " +
+		"detail as b WHERE (a.Drug_name LIKE ? OR a. manufacturer LIKE ? OR a." +
+		"provider_name LIKE ? OR b. approval LIKE ?) AND a.wholesale_id = b.wholesale_id"
 
 	_drugs = "SELECT * FROM `drug` LIMIT ? OFFSET ?"
 )
 
 func (d *Dao) DrugCount(ctx context.Context, key string) (count int32, err error) {
-	err = d.db.QueryRow(ctx, _count, /*like(key),*/ like(key), like(key), like(key)).Scan(&count)
+	err = d.db.QueryRow(ctx, _count, like(key), like(key), like(key), like(key)).Scan(&count)
 	log.Info("count: %d", count)
 	if err != nil {
 		log.Error("d.DrugCount() error(%v)", err)
@@ -34,7 +30,7 @@ func (d *Dao) DrugCount(ctx context.Context, key string) (count int32, err error
 
 func (d *Dao) DrugQuery(ctx context.Context, key string, pageSize, pageNum int32) (ds []*pb.Drug,
 	err error) {
-	rows, err := d.db.Query(ctx, _drug, /*like(key),*/ like(key), like(key), like(key), pageSize,
+	rows, err := d.db.Query(ctx, _drug, like(key), like(key), like(key), like(key), pageSize,
 		pageSize*(pageNum-1))
 	if err != nil {
 		log.Error("d.DrugQuery() error(%v)", err)
@@ -47,7 +43,7 @@ func (d *Dao) DrugQuery(ctx context.Context, key string, pageSize, pageNum int32
 		rows.Scan(&d.WholesaleId, &d.Level0, &d.Level1, &d.Level2, &d.DrugName,
 			&d.ProviderId, &d.ProviderName, &d.Specification, &d.Unit,
 			&d.Manufacturer, &d.ValidDate, &d.ChainPrice, &d.DisPrice,
-			&d.MinPrice, &d.MaxPrice, &d.OldPrice, &d.Price, /*&d.ApprovalNumber,*/ )
+			&d.MinPrice, &d.MaxPrice, &d.OldPrice, &d.Price, &d.ApprovalNumber)
 		ds = append(ds, d)
 	}
 	return
